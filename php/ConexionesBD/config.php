acdc73b27973aaa4df8442c9dd9a955b31023f42
MiguelMatamoros2000 <54284480+MiguelMatamoros2000@users.noreply.github.com>
2020-03-11 11:57:36 -0600

    Update config.php

--- php/ConexionesBD/config.php
+++ php/ConexionesBD/config.php
@@ -3,7 +3,7 @@
     define('KEY','treck');
     define('COD','AES-128-ECB');
     define('HOST', 'localhost');//redireccion al localhost por dns hosting
-    define('USER', 'juan');//nombre de usuario
+    define('USER', 'miguel');//nombre de usuario
     define('PASS', 'clave');//contraseña
     define('DBNAME', 'restaurante');//nombre de base de datos
 ?>
